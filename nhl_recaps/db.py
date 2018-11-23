adacebff8c32fe4460be55c0b8e500ccecdfc64f
Jamie Lowry <jmelowry@gmail.com>
2018-11-23 13:13:57 -0800

    added search function to db

--- nhl_recaps/db.py
+++ nhl_recaps/db.py
@@ -25,7 +25,15 @@ class RecapDb:
         self.c.executemany('INSERT INTO recaps VALUES (?,?,?,?,?)', value)
         self.conn.commit()
 
+    def search_value(self, value):
+        query = self.c.execute("SELECT * FROM recaps where date = ?", (value,))
+        rows = query.fetchall()
+        for row in rows:
+            print(row)
+            # if row[0] == value:
+            #     print(row[0])
 
 if __name__ == '__main__':
     data = RecapDb('recaps.db')
-    data.insert_value([('11/1/2018', 'ducks', 'rangers', 'sad game', '3-2')])
+    data.insert_value([('11/4/2018', 'ducks', 'rangers', 'sad game', '3-2')])
+    data.search_value("11/2/2018")
