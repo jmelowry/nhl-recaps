37ad30e231e4f0fdd39d1b8d3ce42023ac2ff29b
Jamie Lowry <jmelowry@gmail.com>
2019-04-27 23:09:54 -0700

    added packages

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'selenium', 'beautifulsoup4',
+    'selenium', 'beautifulsoup4', 'dateparser'
 ]
 
 # What packages are optional?
